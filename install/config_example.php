4f572447ed94b3900b9c0e6681dbff5c137db692
Nick Janssen <info@nickjanssen.com>
2013-05-05 11:05:22 +0200

    feat(config): move example config to install/ dir, ignore config.php

---
+++ install/config_example.php
@@ -0,0 +1,17 @@
+<?php
+
+$mysql_hostname = "localhost";
+$mysql_user = "root";
+$mysql_password = "next";
+$mysql_db = "ironbane";
+
+$ironbane_hostname = "localhost";
+$ironbane_port = 8080;
+
+// If you installed the client inside c:/xampp/htdocs/ibclient
+// you would fill in /ibclient/ here
+// Basically everything that comes behind the IP address
+// of your server (usually localhost)
+$ironbane_root_directory = "/ironbane/";
+
+?>
