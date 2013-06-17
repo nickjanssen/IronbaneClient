bfff6c128d990be91f2125e1929bb20fad120f7f
Ben Sparks <bsparks@unicon.net>
2013-06-17 16:45:20 -0700

    couple of punctuation things

--- plugins/game/js/ng/app.js
+++ plugins/game/js/ng/app.js
@@ -38,9 +38,9 @@ IronbaneApp.directive('chatWindow', ['$log', function($log) {
     // logic for all of the different types of messages that are supported
     var templates = {
         join: '<div><span class="name {{ data.user.rank }}">{{ data.user.name }}</span> has joined the game!</div>',
-        died: '<div><span class="name {{ data.victim.rank }}">{{ data.victim.name }}</span> was {{ getDeathMsg() }} by <span class="name {{ data.killer.rank }}">{{ data.killer.name }}</span>',
+        died: '<div><span class="name {{ data.victim.rank }}">{{ data.victim.name }}</span> was {{ getDeathMsg() }} by <span class="name {{ data.killer.rank }}">{{ data.killer.name }}.</span>',
         leave: '<div><span class="name {{ data.user.rank }}">{{ data.user.name }}</span> has left the game.</div>',
-        say: '<div><span class="name {{ data.user.rank }}"><{{ data.user.name }}></span><span ng-bind-html="data.message"></span></div>',
+        say: '<div><span class="name {{ data.user.rank }}"><{{ data.user.name }}></span> <span ng-bind-html="data.message"></span></div>',
         "announce": '<div class="message" ng-style="{color: data.message.color}" ng-bind-html="data.message.text"></div>',
         "announce:personal": '<div class="message" ng-style="{color: data.message.color}" ng-bind-html="data.message.text"></div>',
         "announce:mods": '<div class="message" ng-style="{color: data.message.color}" ng-bind-html="data.message.text"></div>',
