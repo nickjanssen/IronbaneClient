17e866b0879635e342c183570cbef21ed70a30ec
Ben Sparks <bsparks@unicon.net>
2013-06-29 23:19:48 -0700

    fix for dropItem;clean up soundHandler to try to debug preload

--- plugins/game/js/Engine/SoundHandler.js
+++ plugins/game/js/Engine/SoundHandler.js
@@ -16,305 +16,265 @@
 */
 
 var SoundHandler = Class.extend({
-  Init: function() {
+    soundList: {
+        "theme": {
+            file: "music/ib_theme.mp3",
+            volume: 0.5,
+            // volume:0,
+            loops: 999,
+            preload: true
+        },
 
-    // Start SoundManager2
-    soundManager.setup({
-      url: ironbane_root_directory + 'plugins/game/flash/',
-      flashVersion: 9,
-      useFlashBlock: false,
-      preferFlash: false,
-      onready: function() {
-        // Ready to use; soundManager.createSound() etc. can now be called.
-        soundHandler.Preload();
-      }
-    });
+        "castle": {
+            file: "music/castle.mp3",
+            volume: 0.5,
+            // volume:0,
+            loops: 1
+        },
 
-    this.loadedMainMenuMusic = false;
+        "tutorial": {
+            file: "music/tutorial.mp3",
+            volume: 0.5,
+            // volume:0,
+            loops: 1
+        },
 
-    this.sounds = {};
+        "ib2": {
+            file: "music/IRONBANE 2.mp3",
+            volume: 0.5,
+            // volume:0,
+            loops: 1
+        },
 
-  },
-  Preload: function() {
+        "ib4": {
+            file: "music/IRONBANE 4.mp3",
+            volume: 0.5,
+            // volume:0,
+            loops: 1
+        },
 
-    this.soundList = {
+        "ib5": {
+            file: "music/IRONBANE 5.mp3",
+            volume: 0.5,
+            // volume:0,
+            loops: 1
+        },
 
-      "theme": {
-        file: "music/ib_theme.mp3",
-        volume: 0.5,
-        // volume:0,
-        loops: 999
-      },
+        "underground": {
+            file: "music/underground.mp3",
+            volume: 0.5,
+            // volume:0,
+            loops: 1
+        },
 
-      "castle": {
-        file: "music/castle.mp3",
-        volume: 0.5,
-        // volume:0,
-        loops: 1
-      },
+        "click": "ui/click.wav",
 
-      "tutorial": {
-        file: "music/tutorial.mp3",
-        volume: 0.5,
-        // volume:0,
-        loops: 1
-      },
-
-      "ib2": {
-        file: "music/IRONBANE 2.mp3",
-        volume: 0.5,
-        // volume:0,
-        loops: 1
-      },
-
-      "ib4": {
-        file: "music/IRONBANE 4.mp3",
-        volume: 0.5,
-        // volume:0,
-        loops: 1
-      },
-
-      "ib5": {
-        file: "music/IRONBANE 5.mp3",
-        volume: 0.5,
-        // volume:0,
-        loops: 1
-      },
-
-      "underground": {
-        file: "music/underground.mp3",
-        volume: 0.5,
-        // volume:0,
-        loops: 1
-      },
-
-      "click": "ui/click.wav",
+        "switch": "misc/switch.wav",
+        "bag1": "misc/bag1.wav",
+        "bag2": "misc/bag2.wav",
+        "drop": "misc/drop.ogg",
+        "drop1": "misc/drop1.wav",
+        "drop2": "misc/drop2.wav",
+        // "enterGame": "misc/enterGame.ogg",
 
 
-      "switch": "misc/switch.wav",
-      "bag1": "misc/bag1.wav",
-      "bag2": "misc/bag2.wav",
-      "drop": "misc/drop.ogg",
-      "drop1": "misc/drop1.wav",
-      "drop2": "misc/drop2.wav",
-      // "enterGame": "misc/enterGame.ogg",
+        "equipSword1": "equip/equipSword1.wav",
+        "equipSword2": "equip/equipSword2.wav",
+        "equipSword3": "equip/equipSword3.wav",
+        "equip1": "equip/equip1.wav",
+        "equip2": "equip/equip2.wav",
 
+        "splash1": "environment/splash1.wav",
+        "splash2": "environment/splash2.wav",
 
-      "equipSword1": "equip/equipSword1.wav",
-      "equipSword2": "equip/equipSword2.wav",
-      "equipSword3": "equip/equipSword3.wav",
-      "equip1": "equip/equip1.wav",
-      "equip2": "equip/equip2.wav",
+        // "arrowHit1": "battle/arrowHit01.wav",
+        // "arrowHit2": "battle/arrowHit02.wav",
+        // "arrowHit3": "battle/arrowHit03.wav",
 
-      "splash1": "environment/splash1.wav",
-      "splash2": "environment/splash2.wav",
+        "swing1": "battle/swing1.wav",
+        "swing2": "battle/swing2.wav",
+        "swing3": "battle/swing3.wav",
+        "hit1": "battle/hit1.wav",
+        "hit2": "battle/hit2.wav",
+        "hit3": "battle/hit3.wav",
+        "die1": "battle/die1.wav",
+        "die2": "battle/die2.wav",
+        "die3": "battle/die3.wav",
 
-      // "arrowHit1": "battle/arrowHit01.wav",
-      // "arrowHit2": "battle/arrowHit02.wav",
-      // "arrowHit3": "battle/arrowHit03.wav",
+        "fireStaff": "battle/fireStaff.wav",
+        "fireArrow": "battle/fireArrow.wav",
 
-      "swing1": "battle/swing1.wav",
-      "swing2": "battle/swing2.wav",
-      "swing3": "battle/swing3.wav",
-      "hit1": "battle/hit1.wav",
-      "hit2": "battle/hit2.wav",
-      "hit3": "battle/hit3.wav",
-      "die1": "battle/die1.wav",
-      "die2": "battle/die2.wav",
-      "die3": "battle/die3.wav",
+        "jump1": "player/jump/Player_Jump_01.wav",
+        "getItem": "player/GetItem/PLAYER_GET_ITEM_01.wav",
+        "regenHealth": "player/RegenHealth/PLAYER_REGEN_HEALTH_03.wav",
+        "getCoin": "player/GetCoins/PLAYER_GET_COINS_01.wav",
+        "takeDamage": "player/TakeDamage/PLAYER_TAKE_DAMAGE_03.wav",
 
-      "fireStaff": "battle/fireStaff.wav",
-      "fireArrow": "battle/fireArrow.wav",
+        "greenSlime1": "NPCs/GreenSlime/01.wav",
+        "greenSlime2": "NPCs/GreenSlime/02.wav",
+        "greenSlime3": "NPCs/GreenSlime/03.wav",
 
-      "jump1": "player/jump/Player_Jump_01.wav",
-      "getItem": "player/GetItem/PLAYER_GET_ITEM_01.wav",
-      "regenHealth": "player/RegenHealth/PLAYER_REGEN_HEALTH_03.wav",
-      "getCoin": "player/GetCoins/PLAYER_GET_COINS_01.wav",
-      "takeDamage": "player/TakeDamage/PLAYER_TAKE_DAMAGE_03.wav",
+        "fireLongbow": "Weapons/WEAPON_LONG_BOW_01.wav",
+        "fireAcidstaff": "Weapons/WEAPON_ACID_STAFF_02.wav",
 
-      "greenSlime1": "NPCs/GreenSlime/01.wav",
-      "greenSlime2": "NPCs/GreenSlime/02.wav",
-      "greenSlime3": "NPCs/GreenSlime/03.wav",
+        "atmosDay": "Atmos/ATMOS_WORLD_DAY.wav",
+        "atmosNight": "Atmos/ATMOS_WORLD_NIGHT.wav",
 
+        "dirtFootStep1": "player/Footsteps/Dirt/01.wav",
+        "dirtFootStep2": "player/Footsteps/Dirt/02.wav",
+        "dirtFootStep3": "player/Footsteps/Dirt/03.wav",
+        "dirtFootStep4": "player/Footsteps/Dirt/04.wav",
+        "dirtFootStep5": "player/Footsteps/Dirt/05.wav",
+        "dirtFootStep6": "player/Footsteps/Dirt/06.wav",
+        "grassFootStep1": "player/Footsteps/Grass/01.wav",
+        "grassFootStep2": "player/Footsteps/Grass/02.wav",
+        "grassFootStep3": "player/Footsteps/Grass/03.wav",
+        "grassFootStep4": "player/Footsteps/Grass/04.wav",
+        "grassFootStep5": "player/Footsteps/Grass/05.wav",
+        "grassFootStep6": "player/Footsteps/Grass/06.wav",
+        "woodFootStep1": "player/Footsteps/Wood/01.wav",
+        "woodFootStep2": "player/Footsteps/Wood/02.wav",
+        "woodFootStep3": "player/Footsteps/Wood/03.wav",
+        "woodFootStep4": "player/Footsteps/Wood/04.wav",
+        "woodFootStep5": "player/Footsteps/Wood/05.wav",
+        "woodFootStep6": "player/Footsteps/Wood/06.wav",
+        "stoneFootStep1": "player/Footsteps/Stone/01.wav",
+        "stoneFootStep2": "player/Footsteps/Stone/02.wav",
+        "stoneFootStep3": "player/Footsteps/Stone/03.wav",
+        "stoneFootStep4": "player/Footsteps/Stone/04.wav",
+        "stoneFootStep5": "player/Footsteps/Stone/05.wav",
+        "stoneFootStep6": "player/Footsteps/Stone/06.wav",
+        "bubble1": "inventory/bubble1.wav",
+        "bubble2": "inventory/bubble2.wav",
+        "bubble3": "inventory/bubble3.wav",
 
-      "fireLongbow": "Weapons/WEAPON_LONG_BOW_01.wav",
-      "fireAcidstaff": "Weapons/WEAPON_ACID_STAFF_02.wav",
+        "IBfire": "NPCs/IRONBANE/Fireattack.wav",
+        "IBbreath1": "NPCs/IRONBANE/breath1.wav",
+        "IBbreath2": "NPCs/IRONBANE/breath2.wav",
+        "IBgrowl": "NPCs/IRONBANE/growl3.wav",
+        "IBattack": "NPCs/IRONBANE/attack2.wav",
 
-      "atmosDay": "Atmos/ATMOS_WORLD_DAY.wav",
-      "atmosNight": "Atmos/ATMOS_WORLD_NIGHT.wav",
+        // "step1": "step/grass1.wav",
+        // "step2": "step/grass2.wav",
+        // "stepWater1": "step/water1.wav",
+        // "stepWater2": "step/water2.wav",
+        "jump": "fighter/jump.wav",
+        //      "race": "battle/02_-_rage_racer.mp3",
+        //      "splash": "battle/splash.ogg",
 
+        "placeholder": "placeholder"
+    },
+    sounds: {},
+    loadedMainMenuMusic: false,
+    Init: function() {
+        var self = this;
 
-      "dirtFootStep1": "player/Footsteps/Dirt/01.wav",
-      "dirtFootStep2": "player/Footsteps/Dirt/02.wav",
-      "dirtFootStep3": "player/Footsteps/Dirt/03.wav",
-      "dirtFootStep4": "player/Footsteps/Dirt/04.wav",
-      "dirtFootStep5": "player/Footsteps/Dirt/05.wav",
-      "dirtFootStep6": "player/Footsteps/Dirt/06.wav",
-      "grassFootStep1": "player/Footsteps/Grass/01.wav",
-      "grassFootStep2": "player/Footsteps/Grass/02.wav",
-      "grassFootStep3": "player/Footsteps/Grass/03.wav",
-      "grassFootStep4": "player/Footsteps/Grass/04.wav",
-      "grassFootStep5": "player/Footsteps/Grass/05.wav",
-      "grassFootStep6": "player/Footsteps/Grass/06.wav",
-      "woodFootStep1": "player/Footsteps/Wood/01.wav",
-      "woodFootStep2": "player/Footsteps/Wood/02.wav",
-      "woodFootStep3": "player/Footsteps/Wood/03.wav",
-      "woodFootStep4": "player/Footsteps/Wood/04.wav",
-      "woodFootStep5": "player/Footsteps/Wood/05.wav",
-      "woodFootStep6": "player/Footsteps/Wood/06.wav",
-      "stoneFootStep1": "player/Footsteps/Stone/01.wav",
-      "stoneFootStep2": "player/Footsteps/Stone/02.wav",
-      "stoneFootStep3": "player/Footsteps/Stone/03.wav",
-      "stoneFootStep4": "player/Footsteps/Stone/04.wav",
-      "stoneFootStep5": "player/Footsteps/Stone/05.wav",
-      "stoneFootStep6": "player/Footsteps/Stone/06.wav",
-
-
-      "bubble1": "inventory/bubble1.wav",
-      "bubble2": "inventory/bubble2.wav",
-      "bubble3": "inventory/bubble3.wav",
-
-      "IBfire": "NPCs/IRONBANE/Fireattack.wav",
-      "IBbreath1": "NPCs/IRONBANE/breath1.wav",
-      "IBbreath2": "NPCs/IRONBANE/breath2.wav",
-      "IBgrowl": "NPCs/IRONBANE/growl3.wav",
-      "IBattack": "NPCs/IRONBANE/attack2.wav",
-
-      // "step1": "step/grass1.wav",
-      // "step2": "step/grass2.wav",
-      // "stepWater1": "step/water1.wav",
-      // "stepWater2": "step/water2.wav",
-
-      "jump": "fighter/jump.wav",
-
-      //      "race": "battle/02_-_rage_racer.mp3",
-      //      "splash": "battle/splash.ogg",
-
-
-      "placeholder": "placeholder"
-    };
-    var self = this;
-    for (var s in this.soundList) {
-      console.log(s);
-      if (typeof this.soundList[s] !== 'object') {
-        this.soundList[s] = {
-          file: this.soundList[s]
-        };
-      }
-
-      if (!ISDEF(this.soundList[s].volume)) {
-        
-        this.soundList[s].volume = 0.3;
-      }
-
-       (function(s) {
-        self.sounds[s] = soundManager.createSound({
-          id: s,
-          url: ironbane_root_directory + 'plugins/game/sound/' + self.soundList[s].file,
-          autoLoad: true,
-          onload: function(success) {
-
-            if (success) {
-              console.log("Loaded sound "+s);
-              self.OnLoad(s);
+        // Start SoundManager2
+        soundManager.setup({
+            url: ironbane_root_directory + 'plugins/game/flash/',
+            flashVersion: 9,
+            useFlashBlock: false,
+            preferFlash: false,
+            onready: function() {
+                // Ready to use; soundManager.createSound() etc. can now be called.
+                self.Preload();
             }
-          }
         });
-      })(s);
+    },
+    Preload: function() {
+        var self = this;
+        _.each(self.soundList, function(sound, key) {
+            if(!_.isObject(sound)) {
+                sound = {file: sound};
+            }
+            sound.volume = sound.volume || 0.3;
+            self.sounds[key] = soundManager.createSound({
+                id: key,
+                url: ironbane_root_directory + 'plugins/game/sound/' + sound.file,
+                autoLoad: !!sound.preload,
+                onload: function(success) {
+                    if(success) {
+                        self.OnLoad(key);
+                    }
+                }
+            });
+        });
+    },
+    FadeOut: function(sound, time) {
+        var self = this;
+
+        this.PlayOnce(sound);
+
+        var tween = new TWEEN.Tween({volume: 100})
+                .to({volume: 0}, time)
+                .onUpdate(function() {
+                    self.SetVolume(sound, this.volume);
+                }).start();
+    },
+    SetVolume: function(sound, volume) {
+        volume *= this.soundList[sound].volume;
+
+        soundManager.setVolume(sound, volume);
+    },
+    FadeIn: function(sound, time) {
+        var self = this;
+        this.PlayOnce(sound);
+
+        var tween = new TWEEN.Tween({volume: 0})
+                .to({volume: 100}, time)
+                .onUpdate(function() {
+                        self.SetVolume(sound, this.volume);
+                }).start();
+    },
+    OnLoad: function(sound) {
+        if (sound === "theme") {
+            this.loadedMainMenuMusic = true;
+        }
+    },
+    PlayOnce: function(sound, position) {
+        if (!hudHandler.allowSound) {
+            return;
+        }
+
+        if (this.sounds[sound].playState !== 0) {
+          return;
+        }
+
+        this.Play(sound, position);
+    },
+    Play: function(sound, position) {
+        if (!hudHandler.allowSound) {return;}
+
+        if (!(sound in this.sounds)) {
+            ba('Sound \'' + sound + '\' does not exist!');
+            return;
+        }
+
+        var distance = 0;
+
+        if (position) {
+            distance = terrainHandler.GetReferenceLocation().subSelf(position).length();
+            distance = Math.pow(distance, 1);
+        }
+
+        //bm("distance: "+distance);
+
+        var volume = distance / 20;
+        volume = 1 - volume.clamp(0, 1);
+        volume = volume * 100;
+        volume = volume.clamp(0, 100);
+        //bm("volume : "+volume );
+
+        this.SetVolume(sound, volume);
+        //soundManager.setPan(sound, 80);
+
+        this.sounds[sound].play({
+            loops: this.soundList[sound].loops
+        });
+    },
+    StopAll: function() {
+        soundManager.stopAll();
     }
-
-     
-
-  },
-  FadeOut: function(sound, time) {
-
-    this.PlayOnce(sound);
-
-    var tween = new TWEEN.Tween({
-      volume: 100
-    })
-      .to({
-      volume: 0
-    }, time)
-    // .easing( TWEEN.Easing.Elastic.InOut )
-    .onUpdate(function() {
-      soundHandler.SetVolume(sound, this.volume);
-    })
-      .start();
-  },
-  SetVolume: function(sound, volume) {
-
-    volume *= this.soundList[sound]['volume'];
-
-    soundManager.setVolume(sound, volume);
-  },
-  FadeIn: function(sound, time) {
-
-    this.PlayOnce(sound);
-
-    var tween = new TWEEN.Tween({
-      volume: 0
-    })
-      .to({
-      volume: 100
-    }, time)
-    // .easing( TWEEN.Easing.Elastic.InOut )
-    .onUpdate(function() {
-      soundHandler.SetVolume(sound, this.volume);
-    })
-      .start();
-  },
-  OnLoad: function(sound) {
-    if (sound === "theme") {
-      this.loadedMainMenuMusic = true;
-    }
-  },
-  PlayOnce: function(sound, position) {
-
-    if (!hudHandler.allowSound) return;
-
-    if (this.sounds[sound].playState !== 0) return;
-
-    this.Play(sound, position);
-
-  },
-  Play: function(sound, position) {
-
-    if (!hudHandler.allowSound) return;
-
-
-    if (!ISDEF(this.sounds[sound])) {
-      ba('Sound \'' + sound + '\' does not exist!');
-      return;
-    }
-
-    var distance = 0;
-
-    if (position) {
-      distance = terrainHandler.GetReferenceLocation().subSelf(position).length();
-      distance = Math.pow(distance, 1);
-    }
-
-    //bm("distance: "+distance);
-
-    var volume = distance / 20;
-    volume = 1 - volume.clamp(0, 1);
-    volume = volume * 100;
-    volume = volume.clamp(0, 100);
-    //bm("volume : "+volume );
-
-
-    soundHandler.SetVolume(sound, volume);
-    //soundManager.setPan(sound, 80);
-
-    this.sounds[sound].play({
-      loops: this.soundList[sound].loops
-    });
-  },
-  StopAll: function() {
-    soundManager.stopAll();
-  }
 });
-
 
 var soundHandler = new SoundHandler();
